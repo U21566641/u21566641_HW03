5216d8edfb95864856c08d8c5c8e447fab0660a2
U21566641 <u21566641@tuks.co.za>
2022-05-31 23:54:02 +0200

    Added necessary model and views

---
+++ u21566641_HW03/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace u21566641_HW03.Models
+{
+    public class FileModel
+    {
+        [Display (Name ="File Name")]
+        public String FileName { get; set; }
+    }
+}
